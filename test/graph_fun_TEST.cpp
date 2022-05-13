96a19de9ab97e90f025aa7ec97642fb0f85d2cf9
[Ka3apuH] <[klient.rubin92596@gmail.com]>
2022-05-13 12:51:16 +0300

    добавление тестов

--- test/graph_fun_TEST.cpp
+++ test/graph_fun_TEST.cpp
@@ -1,52 +1,51 @@
 #include <iostream>
 
-#include "priority_queue.h"
+#include "../function_graph/graph.h"
 #include "gtest/gtest.h"
+#include <array>
 
 #include <random>
 
 #include "graph_fun_TEST.h"
 
+
 using namespace h_work;
 using namespace std;
 
-TEST_F(Testing_main_func_priority_queue,add_element){
-
-    unsigned int sizeOfHeap=randomDevice()%103;
-    //чтобы не было часто равно 0.... взял простое число
-
-    for (unsigned int i = 0; i < sizeOfHeap; ++i) {
-        auto element=randomDevice();
-        test_heap.add_element(element);
-        real_heap.push(element);
-    }
-
-    for (unsigned int i = 0; i < sizeOfHeap; ++i) {
-
-        ASSERT_EQ(test_heap.max_elem(),real_heap.top());
-        test_heap.pop();
-        real_heap.pop();
-    }
+TEST(lab_5a,dfs){
+    auto test_graph=graph<7>{{0,1,1,0,0,0,0},
+                             {1,0,0,1,1,0,0},
+                             {1,0,0,1,0,1,1},
+                             {0,1,1,0,1,0,0},
+                             {0,1,0,1,0,0,0},
+                             {0,0,1,0,0,0,0}};
+    auto res=test_graph.dfs(1);
 }
 
-TEST_F(Testing_main_func_priority_queue,add_2_element){
+TEST(lab_5a,dijkstra) {
+    auto test_graph = graph<7>{{0, 2, 1, 4, 0, 6, 0},
+                               {2, 0, 0, 1, 3, 0, 0},
+                               {1, 0, 0, 2, 0, 4, 0},
+                               {4, 1, 2, 0, 0, 0, 1},
+                               {0, 3, 0, 0, 0, 0, 4},
+                               {6, 0, 4, 0, 0, 0, 7},
+                               {0, 0, 0, 1, 4, 7, 0}};
 
-
-    unsigned int sizeOfHeap=randomDevice()%103;
-    //чтобы не было часто равно 0.... взял простое число
-
-    for (unsigned int i = 0; i < sizeOfHeap; ++i) {
-        auto element=randomDevice();
-        test_heap.add_element(element);
-        real_heap.push(element);
-    }
-
-    for (unsigned int i = 0; i < sizeOfHeap; ++i) {
-
-        ASSERT_EQ(test_heap.max_elem(),real_heap.top());
-        test_heap.pop();
-        real_heap.pop();
-    }
+    ASSERT_EQ(test_graph.dijkstra(3), (std::array<int, 7>{3, 1, 2, 0, 4, 6, 1}));
 }
 
+TEST(lab_5a,kruskal) {
+    auto test_graph = graph<8>{{0, 4, 5, 0, 0, 0, 1, 0},
+                               {4, 0, 0, 8, 0, 0, 0, 3},
+                               {5, 0, 0, 0, 6, 0, 0, 0},
+                               {0, 8, 0, 0, 0, 9, 0, 0},
+                               {0, 0, 6, 0, 0, 7, 0, 0},
+                               {0, 0, 0, 9, 7, 0, 0, 10},
+                               {1, 0, 0, 0, 0, 0, 0, 2},
+                               {0, 3, 0, 0, 0, 10, 2, 0},
+                               };
 
+    auto res=test_graph.kruskal();
+
+    //ASSERT_EQ(test_graph.kruskal());
+}
